880587e777c3e885ce667f9f0e5bb458dc63ecf9
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-12-17 22:31:53 +0530

    first commit

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -46,15 +46,3 @@ func (h *UserHandler) TestUser(c *fiber.Ctx) error {
 	//return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "success", "data": fiber.Map{"user": "kks"}})
 	return pkg.SuccessJSON(c, "asgeraraewr", fiber.Map{"user": "kks"})
 }
-
-// varsha shashank madgunik - ambernath - 9869486121
-// ankita manmath mahajan - 9049229828
-// shivani suresh kahlekar - 9011470111
-// archana chokanpale - 9833698526
-// ashvini shivajiappa nagathne - 9673905225
-// dakshyani wadgaokar - 9822901193
-
-// shraddha malipatil - 8390815542
-// vaishnavi pradip nikhle - 9702577988
-// Neha dhulshete - 7385861678, 7887554543, 8459455787
-// Namrata Khake - 9421085651, 9518772187
